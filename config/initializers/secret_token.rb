4a44e8eb1a9ef20c51e602c2cd96b2a70c24eab7
Pawel Lorek <palorek@gmail.com>
2014-09-24 20:42:43 +0200

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SensorServera::Application.config.secret_key_base = '1ef32d327263da13c349c03e5fa10e9164bedc88a27f7565ffd938440bc3a908d80ed9a27fe51947f6f357d54455264721e01139f2a765424ebf2504fd68a28f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SensorServera::Application.config.session_store :cookie_store, key: '_sensor_servera_session'
