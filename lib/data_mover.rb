62d6a1b7fbf8231aef9b950b78ad0ad1bb579b01
Pawel Lorek <palorek@gmail.com>
2014-09-25 01:51:32 +0200

    Minor upgrades and cleanups to the DataMover - checking if all were loaded fixed, and description trimmed

--- lib/data_mover.rb
+++ lib/data_mover.rb
@@ -8,7 +8,7 @@ class DataMover
   def run
     created = 0
     @data.each do |row| 
-      if TempMeasurement.create(row_to_hash(row))
+      if TempMeasurement.new(row_to_hash(row)).save
         created += 1 
       end
     end
@@ -17,14 +17,13 @@ class DataMover
     end
   end
 
-# 2014-09-24 20:20:12,  82501,  8.25, 1,  Zew Pokój Pawła
   def row_to_hash(row)
     {
       measured_at: ActiveSupport::TimeZone["Warsaw"].parse(row[0]),
       raw: row[1],
       measurement: row[2],
       sensor_id: row[3],
-      description: row[4]
+      description: row[4].try(:trim)
     }    
   end
 end
